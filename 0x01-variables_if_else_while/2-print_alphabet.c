9e957aa85b86f65f8433ab49a80531348d7d395c
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-09-09 13:21:33 -0700

    sommy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+return (0);
+}
