290717ce717a8400f4c71f941e68a9facee995e2
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-09-12 10:39:47 -0700

    sommy

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = '0'; i < '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+if (i != j)
+{
+putchar(i);
+putchar(j);
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
