790510384605ffaa8ee9007ecfc0b19f8ba62aa7
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-09-13 15:34:42 -0700

    sommy

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _islower n- function to print alphabett
+*
+* Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+int c;
+if (c = 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
