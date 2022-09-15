9e35079214f68834d56e94827b76e508c3874c6c
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-09-15 14:06:30 -0700

    sommy

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -20,5 +20,9 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
 
 #endif /* MAIN.H */
