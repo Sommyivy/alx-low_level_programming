3bc6079dc257d0742d9b7508ffa14e930f696fce
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-09-15 06:34:18 -0700

    sommy

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <main.h>
+#include <stdio.h>
+/**
+*_islower - Entry point
+*
+*lowercase character. Another cases, shows 0
+*
+*@c: The character in ASCII code
+*
+*Return: 1 for lowercase character. 0 for the rest.
+*/
+int _isupper(int c)
+{
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
