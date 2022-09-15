4c54256092926c4db32db09ee63a5e15712fcfc1
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-09-15 12:31:03 -0700

    sommy

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,21 @@
 #include "main.h"
 #include <ctype.h>
 /**
-*print_numbers - Entry point
+*print_most_numbers - Entry point
 *
 *Return: @c for lowercase character. 0 for the rest.
 */
-void print_numbers(void)
+void print_most_numbers(void)
+
 {
 int a;
 for (a = '0'; a <= '9'; a++)
 {
+if ( a != 2 && a != 3)
+{
 _putchar(a);
 }
-_putchar('$');
+}
+_putchar('\n');
 }
 
