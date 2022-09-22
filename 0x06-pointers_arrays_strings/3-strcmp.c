60ba7b59a67a8c3cc4887eef09e46d197df546b5
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-09-22 07:24:49 -0700

    sommy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strcmp - a function that concatenates two strings
+*@s1: copy to
+*
+*@s2: copy from
+*
+*Return: Always 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+int ptr;
+ptr = strcmp(s1, s2);
+return (ptr);
+}
