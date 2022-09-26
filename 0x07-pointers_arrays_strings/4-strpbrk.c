42cd3caac82c2e3f5af835ff44c481656066dd95
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-09-26 13:05:03 -0700

    sommy

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strpbrk - a function that searches a string for any of a set of bytes.
+*
+*@s : first string
+*
+*@accept : pointer to second string
+*
+*Return: alway return h
+*/
+char *_strpbrk(char *s, char *accept)
+{
+char  *h;
+h = strpbrk(s, accept);
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strstr - a function that searches a string for any of a set of bytes.
+*
+*@haystack : first string
+*
+*@needle : pointer to second string
+*
+*Return: alway return h
+*/
+char *_strstr(char *haystack, char *needle)
+{
+char  *h;
+h = strstr(haystack, needle);
+}
