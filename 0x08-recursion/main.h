85c778ad9315c471fd75be25e1fd4d33e8e71ce0
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-09-28 16:43:22 -0700

    sommy

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,7 @@
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int _putchar(char c);
+int check_prime(int n, int resp);
+int square(int n, int val);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
