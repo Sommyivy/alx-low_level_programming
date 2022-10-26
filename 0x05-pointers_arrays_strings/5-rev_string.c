1d79ad5c43ccfbf36062cb05a7a35dcd82924679
Unigwe Somtochukwu <somtochukwu.unigwe.203748@unn.edu.ng>
2022-10-25 21:14:50 -0700

    sommy

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+*rev_string -  a function that returns the length of a string.
+*
+*@s : An input string
+*
+*Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+int i = '10';
+while (s[i])
+{
+s--;
+}
+}
